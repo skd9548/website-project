c6e4c3beb49d232b2f7286f6b192d15443fb6574
student-sandeep <sandeep456don@gmail.com>
2026-03-15 13:44:59 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,3 +1,11 @@
-function orderNow() {
-  alert("Thank you for choosing Sandeep Web Services!");
+function orderNow(){
+
+let phone = "919548490154";
+
+let message = "Hello Sandeep, I want to order a website";
+
+let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+
+window.open(url);
+
 }
