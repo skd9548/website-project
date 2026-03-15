20ab256c7b01cdb3db58a598a6baa94f1a6eb8cf
student-sandeep <sandeep456don@gmail.com>
2026-03-15 14:59:55 +0530

    Update first.js

--- first.js
+++ first.js
@@ -9,3 +9,18 @@ let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
 window.open(url);
 
 }
+function sendOrder(){
+
+let name = document.querySelector('input[type="text"]').value;
+let email = document.querySelector('input[type="email"]').value;
+let details = document.querySelector('textarea').value;
+
+let phone = "91XXXXXXXXXX";
+
+let message = "New Website Order:%0AName: " + name + "%0AEmail: " + email + "%0ADetails: " + details;
+
+let url = "https://wa.me/" + phone + "?text=" + message;
+
+window.open(url);
+
+}
