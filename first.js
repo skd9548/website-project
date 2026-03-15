a991cc296e541d210c7d548e3fab7860bd2b89b7
student-sandeep <sandeep456don@gmail.com>
2026-03-15 15:08:37 +0530

    Update first.js

--- first.js
+++ first.js
@@ -15,9 +15,9 @@ let name = document.querySelector('input[type="text"]').value;
 let email = document.querySelector('input[type="email"]').value;
 let details = document.querySelector('textarea').value;
 
-let phone = "91XXXXXXXXXX";
+let phone = "919548490154";
 
-let message = "New Website Order:%0AName: " + name + "%0AEmail: " + email + "%0ADetails: " + details;
+let message = "Hello Sandeep,%0AI want to order a website.%0AName: " + name + "%0AEmail: " + email + "%0ADetails: " + details;
 
 let url = "https://wa.me/" + phone + "?text=" + message;
 
